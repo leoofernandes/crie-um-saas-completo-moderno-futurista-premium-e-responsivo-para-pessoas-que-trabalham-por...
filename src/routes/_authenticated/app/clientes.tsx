5c804c836913f8cf7049b85f1b64175b5e029e4a
leoofernandes <leoofernandes19@gmail.com>
2026-09-18 17:06:27 -0300

    Create clientes.tsx

---
+++ src/routes/_authenticated/app/clientes.tsx
@@ -0,0 +1,4 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { CustomersPage } from "@/components/app/Operations";
+
+export const Route = createFileRoute("/_authenticated/app/clientes")({ component: CustomersPage });
